e5a5da8026a6809ae2affdb60926fec694c11979
maeri18 <maevachekma@gmail.com>
2024-10-14 10:53:35 +0200

    commit 14/10/2024

---
+++ README.md
@@ -0,0 +1,174 @@
+# 👩‍💻📸 Object detection and Person tracking with Tello drone
+
+This repository contains two ROS packages. The first one, [tello](https://github.com/snt-arg/Tello-object-detection/tree/main/ros_workspace/tello), performs object detection using a Tello drone's camera by leveraging YOLOv8, a popular framework for object detection, object classification, and more, developed by [Ultralytics](https://github.com/ultralytics). The second one [person_tracking](https://github.com/snt-arg/Tello-object-detection/tree/main/person_tracking) implements drone person tracking. To run the second package, the [tello ros driver](https://github.com/snt-arg/tello_ros_driver) and [hand gesture detector](https://github.com/snt-arg/hand_gestures_plugin) plugins are required.
+
+## 🔍 Table Of Contents
+
+- [🛠 Installation](#installation)
+- [🧑‍💻️🏃 Run code](#run)
+- [👨🏻‍💻📝Implementation](#implementation)
+- [📈✨Improvement prospects](#improvements)
+- [🧾 License](#license)
+
+## 🛠️ Installation <a id="installation"></a>
+We strongly advise running this project on [Ubuntu 22.04](https://ubuntu.com/tutorials/install-ubuntu-desktop#1-overview) to make sure it is compatible with the [ROS distribution](https://docs.ros.org/en/humble/index.html) we used.
+
+Also, make sure that the system used allows for connection to the drone (We encountered problems while trying to connect the drone to [WSL](https://learn.microsoft.com/en-us/windows/wsl/about).)
+
+To settle the environment to run this project on your side, you must:
+- Clone this repository
+  ```sh
+  git clone https://github.com/snt-arg/Tello-object-detection.git
+  ```
+  You need to have [git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git) installed.
+  
+- Install the requirements specified in [requirements.txt](https://github.com/snt-arg/Tello-object-detection/blob/main/requirements.txt)\
+  If you possess, [pip](https://pypi.org/project/pip/), you can run the command
+  ```sh
+  pip install -r requirements.txt
+  ``` 
+  Even if you do not want to install the packages by using this file, we still recommend you to have a look on the versions of the packages we used, to avoid incompatibility errors.
+
+- Install ROS 2:\
+  For this project, we used ROS 2 Humble Hawksbill. The platforms supported are Ubuntu 22.04, RHEL-8, Windows 10, and macOS(building from source).
+  To install on Ubuntu 22.04 from binary packages(recommended), follow [this tutorial](https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debians.html).
+  For the other platforms, more information is found [here](https://docs.ros.org/en/humble/Installation.html).
+  
+- Install Tello driver and hand gesture packages:\
+  These are two ROS packages mandatory to possess to run the [person_tracking](https://github.com/snt-arg/Tello-object-detection/tree/main/person_tracking) package.
+  You must clone them in the same ROS workspace as [person_tracking](https://github.com/snt-arg/Tello-object-detection/tree/main/person_tracking).\
+  
+  Tello driver:
+  ```sh
+  git clone https://github.com/snt-arg/tello_ros_driver.git
+  ```
+  Hand gesture plugin:
+  ```sh
+  git clone https://github.com/snt-arg/hand_gestures_plugin.git
+  ```
+
+  NB:
+   * YOLOv8 :\
+        YOLOv8 should be installed if you installed all packages in [requirements.txt](https://github.com/snt-arg/Tello-object-detection/blob/main/requirements.txt).\
+      If not, you can run
+      ```sh
+      pip install ultralytics
+      ```
+      However, we recommend that you install all the packages in the requirement file to avoid errors.\
+        Please note that to run a code that uses YOLOv8, you should possess a Python version >=3.8, and PyTorch >= 1.8.\
+        [For more information on how to install YOLOv8](https://github.com/ultralytics/ultralytics?tab=readme-ov-file#documentation).
+    
+    * DJITelloPy:\
+       It should be installed if you installed packages in [requirements.txt](https://github.com/snt-arg/Tello-object-detection/blob/main/requirements.txt). The [tello](https://github.com/snt-arg/Tello-object-detection/tree/main/ros_workspace/tello) package requires [DJITelloPy](https://github.com/damiafuentes/DJITelloPy) to establish the connection to the drone.
+      It can be installed with
+      ```sh
+      pip install djitellopy
+      ```
+      Again, it is advised to install all requirements from the requirement file.
+
+## 🧑‍💻️🏃 Run code
+This repository is organized into 4 directories:
+
+- *config* where all configuration files are kept.
+- *media* containing some videos on which we ran our object detection module to test its functioning.
+- *ros_workspace* contains a robotic package ([tello](https://github.com/snt-arg/Tello-object-detection/tree/main/ros_workspace/tello)) we implemented to perform object detection with a DJI Tello drone.
+  To run the *tello* package, you have to fulfill all installation requirements listed above.
+  Then in a terminal,
+  - Move to the ros_workspace directory after cloning the repository
+    ```sh
+    cd /path/to/ros_workspace
+    ```
+  - Source your ROS distribution using
+    ```sh
+    source /opt/ros/<ros-distro>/setup.bash
+    ```
+    Replace *\<ros-distro\>* with your ROS distribution name.
+  - Build the Tello package
+    ```sh
+    colcon build --packages-select tello
+    ```
+  - Turn the drone on and check your WIFI connection to make sure that your computer is connected to the drone.
+  - Open three other terminals and still in the workspace repertory, source the ROS distribution and the Tello package in all of them with
+      ```sh
+      source ./install/setup.bash
+      ```
+      Then,
+      * In the first terminal, run the drone's camera publisher node using
+        ```sh
+        ros2 run tello camera_pub
+        ```
+      * In the second one, run the object detection subscriber and republisher node using
+        ```sh
+        ros2 run tello detected_pub
+        ```
+      * In the third terminal, you can use visualization tools such as [rqt_image_view](https://wiki.ros.org/rqt_image_view) to view the image messages published on each topic (*image_raw* for raw images coming from the drone, and *image_detected* for images on which object detection was performed. )
+    
+- *person_tracking* contains the second package [person_tracking](https://github.com/snt-arg/Tello-object-detection/tree/main/person_tracking). This package provides a Tello drone with the functionality of tracking a person in real-time.\
+  To run the package, [YOLOv8](https://github.com/ultralytics/ultralytics), [ROS 2](https://docs.ros.org/en/humble/Installation), the [tello driver](https://github.com/snt-arg/tello_ros_driver) and [hand gesture package](https://github.com/snt-arg/hand_gestures_plugin) must be installed.\
+  - Move to the workspace directory after cloning the repository
+    ```sh
+    cd /path/to/workspace
+    ```
+  - Source your ROS distribution using
+    ```sh
+    source /opt/ros/<ros-distro>/setup.bash
+    ```
+    Replace *\<ros-distro\>* with your ROS distribution name.
+  - Build the packages
+    ```sh
+    colcon build --packages-select tello tello_driver hand_gesture
+    ```
+  - Turn the drone on and check your WIFI connection to make sure that your computer is connected to the drone.
+  - Open five other terminals and still in the workspace repertory, source the ROS distribution and the Tello package in all of them with
+      ```sh
+      source ./install/setup.bash
+      ```
+      Then,
+      * In the first terminal, run the tello driver
+        ```sh
+        ros2 launch tello_driver tello_driver.launch.py
+        ```
+      * In the next three terminals run  the nodes in the person_tracking package
+        ```sh
+        ros2 run person_tracking all_detected_node --ros-args -p standalone:=true
+        ```
+        ```sh
+        ros2 run person_tracking trigger_node --ros-args -p standalone:=true
+        ```
+        ```sh
+        ros2 run person_tracking tracker_node --ros-args -p standalone:=true
+        ```
+      * In the fourth terminal, run the hand gesture recognizer
+        ```sh
+        ros2 launch hand_gestures hand_gestures_launch.py run_annotator:=true
+        ```
+      * You can also use visualization tools such as [rqt_image_view](https://wiki.ros.org/rqt_image_view) to view the image messages published on each topic (*image_raw* for raw images coming from the drone, and *all_detected* for images on which person detection was performed.)
+    
+   
+## 👨🏻‍💻📝Implementation <a id="implementation"></a>
+- [tello](https://github.com/snt-arg/Tello-object-detection/tree/main/ros_workspace/tello)\
+  This project uses a publisher/subscriber architecture to detect objects on a drone's camera video.
+  The publisher node [camera_publisher.py](https://github.com/snt-arg/Tello-object-detection/blob/main/ros_workspace/tello/tello/camera_publisher.py) is in charge of connecting to the drone (using [DJITelloPy](https://github.com/damiafuentes/DJITelloPy)) and reading video frames from the drone's camera.
+  These frames are then published on a topic name *image_raw*.
+  The subscriber node [sub1.py](https://github.com/snt-arg/Tello-object-detection/blob/main/ros_workspace/tello/tello/sub1.py) receives frames published on *image_raw*, performs object detection on them, and republishes the new frames (with bounding boxes around objects) on *image_detected*.
+
+- [person_tracking](https://github.com/snt-arg/Tello-object-detection/tree/main/person_tracking)\
+
+## 📈✨Improvement prospects <a id="improvements"></a>
+Current improvement prospects are related to the person tracking package.
+
+- Speed of the plugin:\
+  At its current state, while running the package one might experience lagging and a delayed processing of the frames. This inevitably makes the tracking impossible. An important improvement prospect for this package is to optimize the efficiency of the package.
+  
+- Keeping a certain distance from the target:\
+  For now, the package involves lateral movement and rotation of the drone to keep the target person within the field of view of the drone's camera. Hence, the next step is to keep the distance between the drone and the target within a certain range, for better and effective tracking.
+  
+- Safety of the drone and the people around:\
+  One of the most important aspect to consider when dealing with autonomous systems is trust, and trust in autonomous systems is highly linked to safety around the system. Thus, we consider implementing measures to enhance the safety of the drone and the people around it during the tracking.
+  
+- Integration to Tello_ws:\
+  This package is meant to be included in the [Tello_ws](https://github.com/snt-arg/tello_ws/tree/main) as a plugin.
+## 🧾 License <a id="license"></a>
+
+
+
---
+++ person_tracking/person_tracking/bsp4_files/track_person.py
@@ -0,0 +1,393 @@
+#To handle ROS node
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Empty, String
+
+#ROS Twist message import. This message type is used to send commands to the drone.
+from geometry_msgs.msg import Twist
+
+
+#Custom message containing the midpoint of the bounding box surrounding the tracked person
+from person_tracked.msg import PersonTracked, PointMsg
+
+#For image manipulation (OpenCV)
+import cv2
+#import numpy as np
+
+#To convert cv2 images to ROS Image messages
+from cv_bridge import CvBridge
+
+from person_tracking.pid import PIDPoint
+
+from collections import deque
+
+from math import pi
+
+from threading import Thread
+
+
+##NB : all directions : left, right... are from the drone's perspective
+
+class TrackPerson(Node):
+
+    key_pressed_topic = "/key_pressed"
+    person_tracked_topic = "/person_tracked"
+    #bounding_boxes_topic = "/all_bounding_boxes"
+    commands_topic = "/cmd_vel" #carries Twist msgs
+    land_topic = "/land" #carries Empty msgs
+    takeoff_topic = "/takeoff"
+
+
+    #image_height = 480
+    #image_width = 640
+
+    max_length_midpoint_queue = 2
+
+    max_empty_midpoint_before_lost = 10
+
+    
+    def __init__(self,name):
+        #Creating the Node
+        super().__init__(name)
+        
+        #subscribers
+        self.sub_person_tracked = self.create_subscription(PersonTracked,self.person_tracked_topic, self.listener_callback,5)
+
+        self.sub_key_pressed = self.create_subscription(String,self.key_pressed_topic, self.key_pressed_subscriber_callback,5)
+
+        #publishers
+        self.publisher_commands = self.create_publisher(Twist,self.commands_topic,10)
+        self.timer = self.create_timer(0.09, self.commands_callback)
+        #self.timer_1 = self.create_timer(10, self.commands_callback)
+
+        self.publisher_takeoff = self.create_publisher(Empty,self.takeoff_topic,1)
+       
+        self.publisher_land = self.create_publisher(Empty,self.land_topic,1)
+        
+
+        self.pid = PIDPoint((0.5, 0.5)) #middle of the screen for normalized midpoint coordinates
+       
+        self.person_tracked_midpoint = None
+
+        self.prev_midpoint = None 
+
+        self.commands_msg = None
+
+        self.correction = None
+        
+        #Queue to keep the last nonempty midpoints. Help to calculate the trajectory of the person before he got lost
+        self.midpoint_queue = deque(maxlen=self.max_length_midpoint_queue)
+        
+        self.empty_midpoint_count = 0 #variable to count the amount of empty messages received. If this number is higher than a certain number, the person is considered lost.
+        
+        #Counter for self.persont_tracked_midpoint in the update message function.
+        #If the midpoint is the same after a certain number of calls to the function, the connection might be broken. so we send empty command messages
+        self.connection_lost_midpoint_unchanged_counter = 0  
+        
+        self.update_midpoint = False #is true when we receive a new midpoint that is different than (0,0)
+
+        self.key_pressed = None #variable to contain key pressed on the Pygame GUI, either to land or takeoff
+
+        self.flying = True#variable is True when the dron is flying and False if it landed
+
+        self.rotating = False #variable to check if the drone is rotating
+
+        
+
+        
+        
+###########################first subscriber###########################################################################################   
+    def listener_callback(self, msg):
+        """Callback function for the subscriber node (to topic /person_tracked).
+        Receives the midpoint of the bounding box surrounding the person tracked"""
+        self.get_logger().info('Midpoint received')
+        tmp = msg.middle_point
+
+        if self.empty_midpoint(tmp):
+            self.empty_midpoint_count += 1
+            self.get_logger().info("Empty midpoint")
+            self.update_midpoint = False
+        else:
+            self.update_midpoint = True
+            self.person_tracked_midpoint = tmp
+            self.midpoint_queue.append(self.person_tracked_midpoint)
+            self.empty_midpoint_count = 0
+            self.get_logger().info(f'midpoint {self.person_tracked_midpoint}')
+            self.correction = self.pid.compute(self.person_tracked_midpoint)
+
+    def direction_person_lost(self)->str|None:
+        """Function to determine whether the drone should rotate left, right, up or down to find the lost person"""
+        if len(self.midpoint_queue) == self.max_length_midpoint_queue:
+            point_1 = self.midpoint_queue[1] #most recent midpoint
+            point_2 = self.midpoint_queue[0] #previous midpoint
+            slope = ( point_2.y - point_1.y ) / ( point_2.x - point_1.x )
+            if slope >= 0 :
+                if point_1.x >= point_2.x:
+                    return "right"
+                else:
+                    return "left"
+
+            else:
+                if point_1.x >= point_2.x:
+                    return "right"
+                else:
+                    return "left"
+
+            """
+            if slope >= 1:
+                if point_1.y <= point_2.y:
+                    return "down"
+
+                else:
+                    return "up"
+
+            elif slope < 1 and slope > -1:
+
+                if point_1.x <= point_2.x:
+                    return "left"
+
+                else:
+                    return "right"
+
+            else: #slope <-1
+                if point_1.y <= point_2.y:
+                    return "down"
+
+                else:
+                    return "up"
+            """
+
+        else:
+            self.get_logger().info(f"\nNot enough midpoints received yet to predict the person's position\n")
+        
+        return None
+
+
+############################Second subscriber#######################################################################3333
+            
+    def key_pressed_subscriber_callback(self,msg):
+        self.key_pressed = msg.data
+                      
+######################### Publisher #####################################################################################################
+    def commands_callback(self):
+        self.land_takeoff()
+        if self.flying and not self.rotating:
+            #command_thread = Thread(target=self.update_commands).start()
+            self.update_commands()
+            
+
+    def update_commands(self)->None:
+        """This function makes appropriate commands messages in order to keep the tracked person within the camera's field while ensuring safety"""
+        self.commands_msg = Twist()
+        self.commands_msg.linear.x = 0.0
+        self.commands_msg.linear.y = 0.0
+        self.commands_msg.linear.z = 0.0
+        self.commands_msg.angular.x = 0.0
+        self.commands_msg.angular.y = 0.0
+        self.commands_msg.angular.z = 0.0
+
+        self.get_logger().info(f"\nself.person_lost :{self.person_lost()}\n")
+        self.get_logger().info(f"\nself.empty_midpoint_count :{self.empty_midpoint_count}\n")
+
+        
+        if self.person_lost():
+
+            #self.get_logger().info("\nWalaheiiiiiiiiiii\nWalaheiiiiiiiiiii\nWalaheiiiiiiiiiii\n")
+            direction : str = self.direction_person_lost()
+
+            if direction == "left":
+                print("Rotate left") 
+                #self.rotation(pi/5,2*pi)
+                rotation_thread_left = Thread(target=self.rotation,args=(pi/7,2*pi))
+                rotation_thread_left.start()
+                #rotation_thread_left.join()
+            
+            elif direction == "right":
+                print("Rotate right")
+                #self.rotation(-pi/5,2*pi)
+                rotation_thread_right = Thread(target=self.rotation,args=(-pi/7,2*pi))
+                rotation_thread_right.start()  
+                #rotation_thread_right.join()
+
+            else:
+                self.get_logger().info(f'No trajectory can be found')
+
+        else:
+            #check if the connection is still passing (meaning self.person_tracked_midpoint and hence self.empty_midpoint_count are updated in the listener callback)
+            if not self.check_midpoint_changed():
+                self.publisher_commands.publish(self.commands_msg) 
+                return None
+
+            
+            if self.person_tracked_midpoint is not None and self.correction is not None :
+                correction_x, correction_y = self.correction
+                self.get_logger().info(f'Correction x:{correction_x}, y:{correction_y}')
+                #self.get_logger().info(f'midpoint {self.person_tracked_midpoint}')
+
+                if self.person_tracked_midpoint.x < 0.3:#correction_x < -0.6 : #Here I don't put 0 to avoid having the drone always moving
+                    self.get_logger().info("move left") #(a in keyboard mode control station) 
+                    self.commands_msg.linear.y = 0.22
+                     
+                elif self.person_tracked_midpoint.x > 0.7:#correction_x > 0.6 :
+                    self.get_logger().info("move right") #(d in keyboard mode control station )             
+                    self.commands_msg.linear.y = -0.22
+
+            self.publisher_commands.publish(self.commands_msg) 
+        
+        return None
+            #if correction_y < -0.6 :
+            #    print("move down")
+                #print("move up")
+                #if self.move_down > 0:
+            #    self.commands_msg.linear.z -= 0.3
+                #    self.move_down -= 1
+                #else:
+                #    self.commands_msg.linear.z += 0.0
+
+            #elif correction_y > 0.6 :
+            #    print("move up")
+                #print("move down")
+                #if self.move_up > 0:
+            #    self.commands_msg.linear.z += 0.3
+                 #   self.move_up -= 1
+                #else:
+                #    self.commands_msg.linear.z += 0.0
+
+
+    def person_lost(self):
+        """Function to call when someone is lost.
+        Returns true when the person is lost and False else."""  
+        if self.empty_midpoint_count >= self.max_empty_midpoint_before_lost:
+            return True
+        else: 
+            return False    
+ 
+    def rotation(self, angular_speed, target_angle)->None:
+        """Function to send rotation commands to the drone. 
+        Returns True if the drone did a complete rotation (no one was found) and False else"""
+        if self.commands_msg is not None:
+            self.rotating = True
+            current_angle = 0
+            self.commands_msg.angular.z = angular_speed
+
+
+            self.get_logger().info(f"Before rotating,  angular.z is {self.commands_msg.angular.z} and current_angle is {current_angle}")
+
+            t0 = self.get_clock().now()
+
+            while abs(current_angle) <= abs(target_angle): 
+                #self.get_logger().info(f"Found a person? {self.update_midpoint}")
+                if self.update_midpoint:
+                    self.rotating = False
+                    self.empty_midpoint_count = 0
+                    self.get_logger().info(f"While rotating, found a person to track")
+                    return 
+                self.publisher_commands.publish(self.commands_msg) 
+                t1 = self.get_clock().now()
+
+                current_angle = self.commands_msg.angular.z * ((t1-t0).to_msg().sec)
+
+            self.get_logger().info(f"After rotating ,angular.z is {self.commands_msg.angular.z} and current_angle is {current_angle}")
+            self.rotating = False
+            self.publisher_land.publish(Empty()) #land if we found no one after a complete rotation
+            self.flying = False
+    
+    """
+    def move_y(self,speed, target_distance)->None:
+        #Function to move the drone left or right with a PID
+        if self.commands_msg is not None:
+            self.rotating = True
+            current_distance = 0
+            self.commands_msg.linear.y = angular_speed#pi
+
+            t0 = self.get_clock().now()
+
+            while abs(current_distance) < abs(target_distance):
+
+                self.publisher_commands.publish(self.commands_msg) 
+
+                t1 = self.get_clock().now()
+
+                current_distance = self.commands_msg.linear.y * ((t1-t0).to_msg().sec)
+
+            self.get_logger().info(f"After rotating ,angular.z is {self.commands_msg.angular.z} and current_angle is {current_angle}")
+            self.rotating = False
+            self.publisher_land.publish(Empty()) #land if we found no one after a complete rotation
+            self.flying = False
+    """
+
+    def empty_midpoint(self, midpoint):
+        """Function to test if the current midpoint is empty (x==0 and y==0). 
+        Returns True if the midpoint is empty, and false else
+        Precondition : midpoint is not None and is of type PointMsg"""
+        if midpoint.x == 0 and midpoint.y == 0:
+            return True
+        else:
+            return False  
+
+    def land_takeoff(self)->None:
+        """Prompts the drone to land or takeoff, depending on the messages received"""
+        if self.key_pressed is not None:
+            if self.key_pressed == "t":
+                self.publisher_takeoff.publish(Empty())
+                self.flying = True
+                return None
+            if self.key_pressed == "l":
+                self.publisher_land.publish(Empty())
+                self.flying = False
+                return None
+
+    def equal_point_msg(self, p1, p2)->bool:
+        """function to compare to point messages (PointMsg).
+        Returns True if they have the same coordinates, and False else"""
+        if isinstance(p1, PointMsg) and isinstance(p2, PointMsg):
+            return p1.x == p2.x and p1.y == p2.y
+        else:
+            raise TypeError("Error in function equal_point_msg, tried to compare two objects that are not of type PointMsg")
+
+    def check_midpoint_changed(self)->bool:
+        """Function to check if the self.person_tracked_midpoint changed. Returns True if it stayed the same for less than max_empty_midpoint_before_lost, and False if it stayed the same for more.
+        """
+        if self.prev_midpoint is None and self.person_tracked_midpoint is not None:
+            self.prev_midpoint = self.person_tracked_midpoint
+            return True
+
+        elif self.prev_midpoint is not None :
+            if self.equal_point_msg(self.prev_midpoint, self.person_tracked_midpoint):
+                self.connection_lost_midpoint_unchanged_counter += 1
+            else:
+                self.connection_lost_midpoint_unchanged_counter = 0
+        
+        self.prev_midpoint = self.person_tracked_midpoint
+
+        if self.connection_lost_midpoint_unchanged_counter <= self.max_empty_midpoint_before_lost :
+            return True
+        else: 
+            return False
+        
+
+
+
+
+            
+###################################################################################################################################       
+  
+
+
+def main(args=None):
+    #Intialization ROS communication 
+    rclpy.init(args=args)
+    track_person = TrackPerson('Track_Person_node')
+
+    #execute the callback function until the global executor is shutdown
+    rclpy.spin(track_person)
+    
+    track_person.video.release()
+
+    #destroy the node. It is not mandatory, since the garbage collection can do it
+    track_person.destroy_node()
+    
+    rclpy.shutdown()      
+
+
